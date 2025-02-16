4772cc8054c9caebef5ef390260b1084229cbb96
Givi Kesanashvili <Givi.Kesanashvili@outlook.com>
2025-02-16 18:15:10 +0400

    feat: enhance ExternalClientOptions and add extensions
    Update the `ExternalClientOptions` class to include XML documentation
    comments, modify the `Timeout` property with a new default value of 10
    seconds, and add a `DefaultRequestHeaders` property. Introduce the
    `ApplyTo` method for configuring an `HttpClient` instance.
    
    Add the `ExternalClientServiceCollectionExtensions` class to provide
    extension methods for adding and configuring external HTTP clients in
    the service collection. The `AddExternalClient` method facilitates
    registration of external HTTP clients with validation of options.
    
    Related issues: #1

---
+++ src/Dev.ExternalClients/ExternalClientServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Dev.ExternalClients;
+
+/// <summary>
+/// Provides extension methods for adding and configuring external HTTP clients.
+/// </summary>
+public static class ExternalClientServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds and configures an external HTTP client with the specified options.
+    /// </summary>
+    /// <typeparam name="TClient">The type of the client interface.</typeparam>
+    /// <typeparam name="TImplementation">The type of the client implementation.</typeparam>
+    /// <param name="services">The service collection to add the client to.</param>
+    /// <param name="configureOptions">The action to configure the client options.</param>
+    /// <returns>The service collection with the configured client added.</returns>
+    public static IServiceCollection AddExternalClient<TClient, TImplementation>(
+        this IServiceCollection services, Action<ExternalClientOptions> configureOptions)
+        where TClient : class
+        where TImplementation : class, TClient
+    {
+        services.AddOptions<ExternalClientOptions>(typeof(TImplementation).Name)
+            .Configure(configureOptions)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddHttpClient<TClient, TImplementation>((serviceProvider, client) =>
+        {
+            var options = serviceProvider.GetRequiredService<
+                IOptionsSnapshot<ExternalClientOptions>>().Get(typeof(TImplementation).Name);
+
+            options.ApplyTo(client);
+        });
+
+        return services;
+    }
+}
